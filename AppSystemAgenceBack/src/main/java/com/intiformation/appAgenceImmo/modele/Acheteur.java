78dc9cd4fd50bdafe732aab38ecbd725e24ea028
anais leseur <anais.leseur@gmail.com>
2020-08-29 18:02:55 +0200

    Classes modele : Client ; Locataire ; Acheteur

---
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/modele/Acheteur.java
@@ -0,0 +1,20 @@
+package com.intiformation.appAgenceImmo.modele;
+
+import javax.persistence.Entity;
+
+/**
+ * classe modele pour un acheteur. 
+ * classe qui etend Client
+ * @author anais
+ *
+ */
+@Entity
+public class Acheteur extends Client{
+	
+	// ====== PROPRIETES ======
+	
+	// ====== Ctors ======
+	// ====== METHODES ======
+	// ====== Getters / Setters ======
+
+}// end classe Acheteur
---
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/modele/Client.java
@@ -0,0 +1,93 @@
+package com.intiformation.appAgenceImmo.modele;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+/**
+ * classe modele pour un client. 
+ * classe qui etend Personne
+ * @author anais
+ *
+ */
+@Entity
+@PrimaryKeyJoinColumn(name="personne_id",
+				referencedColumnName="id_personne")
+public class Client extends Personne{
+	
+	// ====== PROPRIETES ======
+	
+	private int telephonePrive;
+	
+	// liste des classes standards qui correspondent aux types de biens qui intéressent le client
+	@ManyToMany (cascade= CascadeType.ALL)
+	@JoinTable (name="classe_standard_client_assos",
+				joinColumns=@JoinColumn(name="personne_id"),
+				inverseJoinColumns= @JoinColumn(name="classe_standard_id"))
+	private List<ClasseStandard> listeClassesStandardsInteret;
+
+	
+	// ====== Ctors ======
+	
+	public Client() {
+	}
+
+	// ctor PROPS de Personne
+	public Client(int id_personne, String nom, String prenom, String email, Adresse adresse) {
+		super(id_personne, nom, prenom, email, adresse);
+		// TODO Auto-generated constructor stub
+	}
+	
+	// ctor ALL PROPS 
+	public Client(int id_personne, String nom, String prenom, String email, Adresse adresse,
+			int telephonePrive, List<ClasseStandard> listeClassesStandardsInteret) {
+		super(id_personne, nom, prenom, email, adresse);
+		this.telephonePrive = telephonePrive;
+		this.listeClassesStandardsInteret = listeClassesStandardsInteret;
+	}
+
+	// ctor SANS Id
+	public Client(String nom, String prenom, String email, Adresse adresse,
+			int telephonePrive, List<ClasseStandard> listeClassesStandardsInteret) {
+		super(nom, prenom, email, adresse);
+		this.telephonePrive = telephonePrive;
+		this.listeClassesStandardsInteret = listeClassesStandardsInteret;
+	}
+
+	// ctor SANS Id et Adresse
+	public Client(String nom, String prenom, String email, int telephonePrive, 
+			List<ClasseStandard> listeClassesStandardsInteret) {
+		super(nom, prenom, email);
+		this.telephonePrive = telephonePrive;
+		this.listeClassesStandardsInteret = listeClassesStandardsInteret;
+	}
+	
+	
+	
+	// ====== METHODES ======
+	// ====== Getters / Setters ======
+	
+
+	public int getTelephonePrive() {
+		return telephonePrive;
+	}
+
+	public void setTelephonePrive(int telephonePrive) {
+		this.telephonePrive = telephonePrive;
+	}
+
+	public List<ClasseStandard> getListeClassesStandardsInteret() {
+		return listeClassesStandardsInteret;
+	}
+
+	public void setListeClassesStandardsInteret(List<ClasseStandard> listeClassesStandardsInteret) {
+		this.listeClassesStandardsInteret = listeClassesStandardsInteret;
+	}
+
+
+}// end classe Client
