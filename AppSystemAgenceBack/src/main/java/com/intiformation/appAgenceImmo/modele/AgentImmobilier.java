a6e962d7c0f348fde4951938659c2e5fc1ef3171
anais leseur <anais.leseur@gmail.com>
2020-08-29 18:26:37 +0200

    Update AgentImmobilier.java

--- AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/modele/AgentImmobilier.java
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/modele/AgentImmobilier.java
@@ -21,13 +21,40 @@ public class AgentImmobilier extends Personne {
 	private String motDePasse;
 	
 	
-	
-	
-	
-	
 	// ====== Ctors ======
+	public AgentImmobilier() {
+	}
+	
+	// ctor ALL PROPS 
+	public AgentImmobilier(int id_personne, String nom, String prenom, String email, Adresse adresse, String motDePasse) {
+		super(id_personne, nom, prenom, email, adresse);
+		this.motDePasse = motDePasse;
+	}
+
+	// ctor SANS Adresse 
+	public AgentImmobilier(int id_personne, String nom, String prenom, String email, String motDePasse) {
+		super(id_personne, nom, prenom, email);
+		this.motDePasse = motDePasse;
+	}
+
+	// ctor SANS Id et Adresse
+	public AgentImmobilier(String nom, String prenom, String email, String motDePasse) {
+		super(nom, prenom, email);
+		this.motDePasse = motDePasse;
+	}
+	
+	
+	
 	// ====== METHODES ======
 	// ====== Getters / Setters ======
 
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	
 
 }// end classe AgentImmobilier
