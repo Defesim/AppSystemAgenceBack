b7ee825dc305e63f909444e523bd7c7f28c6f64d
hannah levardon <hlevardon@gmail>
2020-08-30 18:44:32 +0200

    Update IGenericService.java

--- AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/service/IGenericService.java
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/service/IGenericService.java
@@ -1,5 +1,29 @@
 package com.intiformation.appAgenceImmo.service;
 
-public interface IGenericService {
+import java.util.List;
+/**
+ * Interface générique pour le service: 
+ * Contient toutes les méthodes communes à toute les interfaces de service
+ * @author hannahlevardon
+ *
+ * @param <T>
+ */
+public interface IGenericService<T> {
+	
+	public List<T> trouverTout();
+	
+	public T trouverViaId(Long pId); 
+	
+	public T trouverUn(Long pId);
+	
+	public T ajouter(T t);
+	
+	public T modifier(T t); 
+	
+	public void supprimer(T t);
+	
+	public void supprimerViaId(Long pId);
+	
+	
 
-}
+}//end interface
