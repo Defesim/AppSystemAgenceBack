897d799a0477155348c82e4d7e67535a001c7c14
hannah levardon <hlevardon@gmail>
2020-08-30 12:00:49 +0200

    Update BienImmobilierRepository.java

--- AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/dao/BienImmobilierRepository.java
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/dao/BienImmobilierRepository.java
@@ -1,5 +1,26 @@
 package com.intiformation.appAgenceImmo.dao;
 
-public interface BienImmobilierRepository {
+import java.util.List;
 
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.intiformation.appAgenceImmo.modele.BienImmobilier;
+import com.intiformation.appAgenceImmo.modele.ClasseStandard;
+
+/**
+ * 
+ * @author hannahlevardon
+ *
+ */
+@RepositoryRestResource // annotation d'un WS Rest
+public interface BienImmobilierRepository extends JpaRepository<BienImmobilier, Long> {
+	
+	/**
+	 * Méthode de requête qui permet de récupérer une liste de biens immobiliers à partir d'une classe Standard
+	 * @param pClasseStandard
+	 * @return liste de biens immobilier
+	 */
+	public List<BienImmobilier> findByClasseStandard(ClasseStandard classeStandard); 
+
+}//end interface
