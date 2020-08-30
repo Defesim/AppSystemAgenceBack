ccbfbaca8692e0d8cdc85b6c4d16f5cc17eb6808
hannah levardon <hlevardon@gmail>
2020-08-30 18:44:45 +0200

    Create LocationRepository.java

---
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/dao/LocationRepository.java
@@ -0,0 +1,17 @@
+package com.intiformation.appAgenceImmo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.intiformation.appAgenceImmo.modele.Location;
+
+/**
+ * Implementation de la couche DAO pour une location
+ * implementation d'un ws REST 
+ * @author hannahlevardon
+ *
+ */
+@RepositoryRestResource // annotation d'un WS Rest
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+}//end interface
