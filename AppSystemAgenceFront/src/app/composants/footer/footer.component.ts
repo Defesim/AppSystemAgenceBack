36954327981a4d87b3762ac3c200350fba979bd3
kevin wanaverbecq <kevin-wanaverbecq@hotmail.com>
2020-08-31 16:03:00 +0200

    ajout bootstrap jquery / debut footer & header

--- AppSystemAgenceFront/src/app/composants/footer/footer.component.ts
+++ AppSystemAgenceFront/src/app/composants/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  x : number = 0;
+  prefix : string = "href";
+  suffix : string = "ts";
+  constructor(elementRef:ElementRef, renderer:Renderer2) { }
 
   ngOnInit(): void {
+    setInterval(this.DisplayNextColor,2000);
+  }
+
+  DisplayNextColor(){
+    (this.x == 5) ? 0 : this.x+1;
+    console.log(this.x);
+    for (let index = 1; index < 6; index++) {
+      let ElementId = this.prefix + index + this.suffix;
+      let hrefModif = document.getElementById(ElementId);
+      let newClassNumber = this.x + index;
+      if(newClassNumber > 5){
+        newClassNumber = newClassNumber - 5;
+      }
+      hrefModif.className = "Color" + newClassNumber;
+    }
   }
 
 }
