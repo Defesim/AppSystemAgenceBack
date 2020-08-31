1aab0b12efd5b6731be6c76b1870084c05fd494b
Gabriel Gaugain <gabrielthahand@gmail.com>
2020-08-31 10:23:54 +0200

    creation projet angular

---
+++ AppSystemAgenceFront/src/app/composants/clients-proprio/clients-proprio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-proprio',
+  templateUrl: './clients-proprio.component.html',
+  styleUrls: ['./clients-proprio.component.css']
+})
+export class ClientsProprioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
