794aa80a02661acdeb58475f4c197ea701986c75
hannah levardon <hlevardon@gmail>
2020-08-31 18:22:08 +0200

    Update ClasseStandarWebService.java

--- AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/ws_rest/ClasseStandarWebService.java
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/ws_rest/ClasseStandarWebService.java
@@ -1,23 +1,105 @@
 package com.intiformation.appAgenceImmo.ws_rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.intiformation.appAgenceImmo.modele.BienImmobilier;
+import com.intiformation.appAgenceImmo.modele.ClasseStandard;
+import com.intiformation.appAgenceImmo.modele.Personne;
+import com.intiformation.appAgenceImmo.modele.Proprietaire;
+import com.intiformation.appAgenceImmo.service.IClasseStandardService;
+
 /**
-* Implémentation du Web Service Rest pour une classe Standard
-* 
-* URL de base :
-* http://localhost:8080/gestion-agence-immo/classeStandard/*
-* @author hannahlevardon
-*
-*/
+ * Implémentation du Web Service Rest pour une classe Standard
+ * 
+ * URL de base : http://localhost:8080/gestion-agence-immo/classeStandard/*
+ * 
+ * @author hannahlevardon
+ *
+ */
 @RestController // déclaration de la classe comme WS Rest
-@RequestMapping("/classeStandard") 
+@RequestMapping("/classeStandard")
 public class ClasseStandarWebService {
-	
+
 	/*--------------- PROPRIETES ---------------*/
 	// Déclaration de la couche service
-	 
+	@Autowired
+	private IClasseStandardService classeStandardService;
+
+	// Déclaration de la classe pour la config de l'id dans le ws rest
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
+	/**
+	 * Déclaration des setter pour injection Spring 
+	 * 
+	 */
+	public void setClasseStandardService(IClasseStandardService classeStandardService) {
+		this.classeStandardService = classeStandardService;
+	}
+
+	public void setRepositoryRestConfiguration(RepositoryRestConfiguration repositoryRestConfiguration) {
+		this.repositoryRestConfiguration = repositoryRestConfiguration;
+	}
+	
+	/*--------------- METHODES A EXPOSER DANS LE WEB SERVICE ---------------*/
+	
+	/**
+	 * Méthode exposée dans le WS pour ajouter une classe standard
+	 * URL: http://localhost:8080/gestion-agence-immo/classeStandard/save
+	 * @param pClasse
+	 * @return la classe standard à ajouter 
+	 */
+	@PostMapping(value="save")
+	public ClasseStandard ajouterClasseStandard(@RequestBody ClasseStandard pClasse) {
+		return classeStandardService.ajouter(pClasse);
+	}//end ajouterClasseStandard
+	
+	/**
+	 * Méthode exposée dans le WS pour afficher la liste des toutes les classes standards
+	 * URL: http://localhost:8080/gestion-agence-immo/classeStandard/getAll
+	 * @return
+	 */
+	@GetMapping(value="getAll")
+	public List<ClasseStandard> trouverToutesLesClasseStandards(){		
+		return classeStandardService.trouverTout();	
+	}//end trouverToutesLesClasseStandards
+	
+	/**
+	 * Méthode exposée dans le WS pour afficher une classe standard via son Id
+	 * URL: http://localhost:8080/gestion-agence-immo/classeStandard/findById/id-classe
+	 * @return
+	 */
+	@GetMapping(value="/findById/{id}")
+	public ClasseStandard trouverClasseStandardViaId(@PathVariable("id") Long idClasse) {
+		
+		//repositoryRestConfiguration.exposeIdsFor(ClasseStandard.class);	
+		return classeStandardService.trouverViaId(idClasse);
+		
+	}// end trouverClasseStandardViaId
+	
+	
 
 }// end classe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
