f3fe66601c3df7e66072a67d2ec924e4b34f6f41
Gabriel Gaugain <gabrielthahand@gmail.com>
2020-08-31 15:59:20 +0200

    ajout de la page de login

--- AppSystemAgenceFront/src/app/app.module.ts
+++ AppSystemAgenceFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+
+import {FormsModule} from '@angular/forms'
+
 import { HeaderComponent } from './composants/header/header.component';
 import { FooterComponent } from './composants/footer/footer.component';
 import { VisitesComponent } from './composants/visites/visites.component';
 import { ClientsProprioComponent } from './composants/clients-proprio/clients-proprio.component';
+import { LoginComponent } from './composants/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,14 @@ import { ClientsProprioComponent } from './composants/clients-proprio/clients-pr
     HeaderComponent,
     FooterComponent,
     VisitesComponent,
-    ClientsProprioComponent
+    ClientsProprioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
