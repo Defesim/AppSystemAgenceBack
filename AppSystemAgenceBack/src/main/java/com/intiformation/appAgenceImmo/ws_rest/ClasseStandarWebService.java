2bee24bbaad1fbf275473c08c1e4a430cc55dfd4
hannah levardon <hlevardon@gmail>
2020-08-31 17:55:17 +0200

    Create ClasseStandarWebService.java

---
+++ AppSystemAgenceBack/src/main/java/com/intiformation/appAgenceImmo/ws_rest/ClasseStandarWebService.java
@@ -0,0 +1,23 @@
+package com.intiformation.appAgenceImmo.ws_rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* Implémentation du Web Service Rest pour une classe Standard
+* 
+* URL de base :
+* http://localhost:8080/gestion-agence-immo/classeStandard/*
+* @author hannahlevardon
+*
+*/
+@RestController // déclaration de la classe comme WS Rest
+@RequestMapping("/classeStandard") 
+public class ClasseStandarWebService {
+	
+	/*--------------- PROPRIETES ---------------*/
+	// Déclaration de la couche service
+	 
+
+}// end classe
