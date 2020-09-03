1583ff8ed57afa3047f3b07f8919e211f2177a43
kevin wanaverbecq <kevin-wanaverbecq@hotmail.com>
2020-09-03 13:41:35 +0200

    debut acquisition

--- AppSystemAgenceFront/src/app/composants/create/create-acquisition/create-acquisition.component.ts
+++ AppSystemAgenceFront/src/app/composants/create/create-acquisition/create-acquisition.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import des modeles
+import { Acquisition } from 'src/app/modèles/Acquisition'
+import { BienImmobilier } from 'src/app/modèles/BienImmobilier'
+import { AgentImmobilier } from 'src/app/modèles/AgentImmobilier'
+import { Client } from 'src/app/modèles/Client'
+
 @Component({
   selector: 'app-create-acquisition',
   templateUrl: './create-acquisition.component.html',
   styleUrls: ['./create-acquisition.component.css']
 })
 export class CreateAcquisitionComponent implements OnInit {
+
+  acquisition : Acquisition;
+  bienImmobilier : BienImmobilier;
+  agentImmobilier : AgentImmobilier;
+  client : Client;
+
+  idAcquisition:number;
+  typeAcquisition:string;
+  dateAchat: string; //Date
+  prixAchat:number;
+  bienImmobilier:BienImmobilier;
+  agentImmobilier: AgentImmobilier;
+  client:Client;
 
   constructor() { }
 
