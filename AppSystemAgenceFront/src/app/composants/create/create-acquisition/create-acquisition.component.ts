10d9280ede2ff9a7537993876891b1555e2a4c19
hannah levardon <hlevardon@gmail>
2020-09-03 12:26:51 +0200

    Creation composant create acquisition

---
+++ AppSystemAgenceFront/src/app/composants/create/create-acquisition/create-acquisition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-acquisition',
+  templateUrl: './create-acquisition.component.html',
+  styleUrls: ['./create-acquisition.component.css']
+})
+export class CreateAcquisitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
