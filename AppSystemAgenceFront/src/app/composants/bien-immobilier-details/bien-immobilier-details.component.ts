4f57f0eced443044456ae89877701f3541ca5ef1
hannah levardon <hlevardon@gmail>
2020-09-03 12:27:06 +0200

    creation composant bien Immobilier détails

---
+++ AppSystemAgenceFront/src/app/composants/bien-immobilier-details/bien-immobilier-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bien-immobilier-details',
+  templateUrl: './bien-immobilier-details.component.html',
+  styleUrls: ['./bien-immobilier-details.component.css']
+})
+export class BienImmobilierDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
